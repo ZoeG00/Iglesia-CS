198dbe2d970ded826afe9a7df42b6b03e4e991f2
Zoe Guzman <zoeguzman.ok@gmail.com>
2023-09-06 20:06:48 -0300

    fix rutas

--- src/routes/rutas.jsx
+++ src/routes/rutas.jsx
@@ -7,19 +7,19 @@ import Intercesores from "../Pages/Intercesores";
 import Contacto from "../Pages/Contacto";
 import SobreNosotros from "../Pages/SobreNosotros";
 
-import { BrowserRouter as Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default function Rutas() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/Kids" element={<Kids />} />
-      <Route path="/Adolescentes" element={<Adolescentes />} />
-      <Route path="/Jovenes" element={<Jovenes />} />
-      <Route path="/Matrimonios" element={<Matrimonios />} />
-      <Route path="/Intercesores" element={<Intercesores />} />
-      <Route path="/Contacto" element={<Contacto />} />
-      <Route path="/Sobrenosotros" element={<SobreNosotros />} />
-    </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Kids" element={<Kids />} />
+          <Route path="/Adolescentes" element={<Adolescentes />} />
+          <Route path="/Jovenes" element={<Jovenes />} />
+          <Route path="/Matrimonios" element={<Matrimonios />} />
+          <Route path="/Intercesores" element={<Intercesores />} />
+          <Route path="/Contacto" element={<Contacto />} />
+          <Route path="/Sobrenosotros" element={<SobreNosotros />} />
+        </Routes>
   );
 }
